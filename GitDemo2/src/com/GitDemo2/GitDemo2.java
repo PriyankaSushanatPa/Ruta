8835237a77db548e13360c7eebe294e3b26eaf1e
user <user@DESKTOP-LOVFUC1.bbrouter>
2022-04-26 16:47:57 +0530

    By Priyanka

--- GitDemo2/src/com/GitDemo2/GitDemo2.java
+++ GitDemo2/src/com/GitDemo2/GitDemo2.java
@@ -3,13 +3,14 @@ package com.GitDemo2;
 public class GitDemo2
 {
 
-	public void m1()
+	public static void m1()
 	{
 		System.out.println("M1 method");
 	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Main method");
+		m1();
 
 	}
 
